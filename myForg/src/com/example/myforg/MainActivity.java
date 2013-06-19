5954364f53fc7f4f8eb341bcf68468781128f196
liuchunlong <huaxialiuchunlong@163.com>
2013-06-20 00:41:11 +0800

    onclick

--- myForg/src/com/example/myforg/MainActivity.java
+++ myForg/src/com/example/myforg/MainActivity.java
@@ -3,14 +3,11 @@ package com.example.myforg;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -18,5 +15,24 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	TextView Ev1;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {  
+        super.onCreate(savedInstanceState);  
+        setContentView(R.layout.activity_main);
+  
+        Ev1 = (TextView)findViewById(R.id.myText);    
+  
+        Button Btn1 = (Button)findViewById(R.id.button1);//获取按钮资源    
+        Btn1.setOnClickListener(new Button.OnClickListener(){//创建监听    
+        	int a;
+            @Override
+			public void onClick(View v) {    
+                String strTmp = String.valueOf(a++);    
+                Ev1.setText(strTmp);    
+            }    
+  
+        });
+        }
 
 }
