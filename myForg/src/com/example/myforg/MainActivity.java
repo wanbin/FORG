26fe3fef62c464700a36df044e98abf2db0d761f
wanbin <wanbinr@gmail.com>
2013-06-22 00:54:15 +0800

    add wav

--- myForg/src/com/example/myforg/MainActivity.java
+++ myForg/src/com/example/myforg/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.myforg;
 
-import android.os.Bundle;
+import java.util.HashMap;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+@SuppressLint("UseSparseArrays")
 public class MainActivity extends Activity {
-
+	 private SoundPool sp; 
+	 private HashMap<Integer,Integer> spMap; 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -17,10 +24,16 @@ public class MainActivity extends Activity {
 	}
 	TextView Ev1;
 	@Override
-	public void onCreate(Bundle savedInstanceState) {  
+	public void onCreate(Bundle savedInstanceState) { 
+		
+		
         super.onCreate(savedInstanceState);  
         setContentView(R.layout.activity_main);
   
+        sp=new SoundPool(2,AudioManager.STREAM_SYSTEM,0); 
+        spMap = new HashMap<Integer,Integer>(); 
+        
+        spMap.put(1, sp.load(this, R.raw.clap, 1)); 
         Ev1 = (TextView)findViewById(R.id.myText);    
   
         Button Btn1 = (Button)findViewById(R.id.button1);//获取按钮资源    
@@ -30,9 +43,12 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {    
                 String strTmp = String.valueOf(a++);    
                 Ev1.setText(strTmp);    
+//                sp.paly(spMap.get(1)); 
+                sp.play(spMap.get(spMap.get(1)), 50, 50, 1, 1, 1);
             }    
   
         });
         }
+	
 
 }
