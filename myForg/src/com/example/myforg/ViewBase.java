108efbf1cd5ebf96939e3a21920d701c981bd01b
wanbin <wanbinr@gmail.com>
2013-07-06 11:40:53 +0800

    baseView class

---
+++ myForg/src/com/example/myforg/ViewBase.java
@@ -0,0 +1,22 @@
+package com.example.myforg;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.util.Log;
+
+public class ViewBase extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		String className = getTraceInfo();
+		Log.v("tag", "CREATE View " + className + "Success!");
+	}
+
+	public static String getTraceInfo() {
+		StringBuffer sb = new StringBuffer();
+		StackTraceElement[] stacks = new Throwable().getStackTrace();
+		sb.append("class: ").append(stacks[1].getClassName());
+		return sb.toString();
+	}
+}
