65c8e5f100ce5cc6fe5e267b3ee5cd5d80bb3b46
liuchunlong <huaxialiuchunlong@163.com>
2013-07-06 13:59:38 +0800

    new files

---
+++ myForg/src/com/example/myforg/Choice_OneActivity.java
@@ -0,0 +1,26 @@
+package com.example.myforg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @todo:   ²âÊÔ£¬Îğ¶¯£¬Ğ»Ğ»
+ * @author: liuchunlong
+ * @date:   2013-7-1 ÏÂÎç9:46:16
+ */
+public class Choice_OneActivity extends Activity {
+
+	/**
+	 * @override:	   
+	 * @author: 	liuchunlong
+	 * @date:   	2013-7-1 ÏÂÎç9:52:11
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+
+	
+}
---
+++ myForg/src/com/example/myforg/clickActivity.java
@@ -0,0 +1,7 @@
+package com.example.myforg;
+
+import android.app.Activity;
+
+public class clickActivity extends Activity {
+	
+}
