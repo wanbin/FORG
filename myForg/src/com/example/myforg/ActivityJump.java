3fda0a352630ab67b75e0af8af74c462af764133
wanbin <wanbinr@gmail.com>
2013-07-19 01:44:59 +0800

    jump to surfaceview

--- myForg/src/com/example/myforg/ActivityJump.java
+++ myForg/src/com/example/myforg/ActivityJump.java
@@ -1,26 +1,46 @@
 package com.example.myforg;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
-import android.view.animation.AnimationSet;
-import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+
+import com.example.util.ResReader;
+import com.example.view.JumpView;
 
 public class ActivityJump extends ViewBase {
 	private ImageView btnJumpOne;
 	private ImageView btnJumpTwo;
 	private ImageView btnFrog;
 	private AnimationDrawable animDrawable;
-	private LinearLayout linearLayout;
-
-	private int objectLong = 40;
+	JumpView jumpView;
+	private int objectLong = 8;
 	private int mapNow = 0;
+	private SurfaceHolder holder;
+	private Paint paint;
+	private ResReader resReader;
+	private int a;
+	private boolean running = true;
+	private Bitmap back;
+	Thread jumpThread;
+	Paint mPaint;
+	private Bitmap zhaoze01;
+	private Bitmap zhaoze02;
+	private Bitmap zhaoze03;
+	private Bitmap zhaoze04;
+	private Bitmap zhaoze05;
+	private Bitmap zhaoze06;
+	private int jumpStep;
 	private int[] objectplace = { 1, 1, 2, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1,
 			1, 0, 3, 1, 2, 0, 1, 4, 4, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1,
 			0, 3, 1, 5, 1, 2, 5, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1,
@@ -31,7 +51,46 @@ public class ActivityJump extends ViewBase {
 		setContentView(R.layout.activity_jump);
 		btnJumpOne = (ImageView) findViewById(R.id.imageOne);
 		btnJumpTwo = (ImageView) findViewById(R.id.imageTwo);
-		linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+
+		SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surface);
+		surfaceView.setZOrderOnTop(true);
+		surfaceView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+		resReader = new ResReader(this, "zhaoze_objects");
+
+		zhaoze01 = resReader.getImg("objects0001.png");
+		zhaoze02 = resReader.getImg("objects0002.png");
+		zhaoze03 = resReader.getImg("objects0003.png");
+		zhaoze04 = resReader.getImg("objects0004.png");
+		zhaoze05 = resReader.getImg("objects0005.png");
+		zhaoze06 = resReader.getImg("objects0006.png");
+
+		paint = new Paint();
+		paint.setColor(Color.GREEN);
+		paint.setStrokeWidth(3);
+		holder = surfaceView.getHolder();
+
+		jumpStep = 0;
+
+		holder.addCallback(new SurfaceHolder.Callback() {
+			public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2,
+					int arg3) {
+				// TODO Auto-generated method stub
+			}
+
+			public void surfaceCreated(SurfaceHolder arg0) {
+				// TODO Auto-generated method stub
+				jumpThread = new Thread(new JumpThread());
+				jumpThread.start();
+			}
+
+			public void surfaceDestroyed(SurfaceHolder arg0) {
+				// TODO Auto-generated method stub
+			}
+		});
+
+
+
+
 		// linearLayout.setWidth();
 		btnFrog = (ImageView) findViewById(R.id.imageFrog);
 		btnJumpOne.setImageBitmap(getImg("jumpbutton", "jumpbutton10.png"));
@@ -43,18 +102,21 @@ public class ActivityJump extends ViewBase {
 		btnJumpOne.setOnClickListener(new Button.OnClickListener() {// 创建监听
 					@Override
 					public void onClick(View v) {
-						jumpOne();
+						jumpOne(1);
 					}
 				});
 
 		btnJumpTwo.setOnClickListener(new Button.OnClickListener() {// 创建监听
 					@Override
 					public void onClick(View v) {
-						jumpTwo();
+						jumpOne(2);
 					}
 				});
-		initMap(mapNow, objectLong);
-
+		// initMap(mapNow, objectLong);
+		// jumpView = new JumpView(this);
+		// FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(100,
+		// 100);
+		// addContentView(jumpView, params);
 	}
 
 	protected int[] initObject(int startPlace) {
@@ -66,110 +128,109 @@ public class ActivityJump extends ViewBase {
 	}
 
 	protected void initMap(int start, int count) {
-		// int[] map = initObject(start);
-		// gridlayoutMap.removeAllViews();
-		if (linearLayout.getChildCount() == 0) {
-			for (int i = 0; i < objectLong; i++) {
-				ImageView imgMap = new ImageView(this);
-				// imgMap.setTag("btnfrog" + i);
-				Bitmap animDrawable = getImg("zhaoze_objects", "objects000"
-						+ objectplace[i] + ".png");
-				imgMap.setImageBitmap(animDrawable);
-				linearLayout.addView(imgMap, 132, 58);
-			}
+		int[] map = initObject(start);
+		for (int i = 0; i < map.length; i++) {
+
 		}
-			
-		// } else {
-		// for (int i = 0; i < objectLong; i++) {
-		// String tag = "btnfrog" + i;
-		// ImageView imgMap = (ImageView) tableRow
-		// .findViewWithTag(tag);
-		// Bitmap animDrawable = getImg("zhaoze_objects", "objects000"
-		// + map[i] + ".png");
-		// imgMap.setImageBitmap(animDrawable);
-		// }
-		// }
 	}
 
 	protected void moveObject(int oneTwo) {
 
 	}
 
-	protected void jumpOne() {
+	protected void jumpOne(int count) {
 		animDrawable.stop();
 		animDrawable.start();
-
-		AnimationSet as = new AnimationSet(true);
-		TranslateAnimation al = new TranslateAnimation(0, -(mapNow * 132), 0,
-				-(mapNow * 132) - 132, 0, 0, 0,
-				0);
-		al.setDuration(280);
-		TranslateAnimation al2 = new TranslateAnimation(0, -132, 0, 0, 0, 0, 0,
-				0);
-		al2.setDuration(0);
-		al2.setStartOffset(280);
-		as.addAnimation(al);
-		// as.addAnimation(al2);
-		as.setFillAfter(true);
-		mapNow += 1;
-
-		al.setAnimationListener(new AnimationListener() {
-			@Override
-			public void onAnimationStart(Animation animation) {
-				btnJumpOne.setClickable(false);
-				btnJumpTwo.setClickable(false);
-			}
-
-			@Override
-			public void onAnimationRepeat(Animation animation) {
-			}
-
-			@Override
-			public void onAnimationEnd(Animation animation) {
-				// initMap(mapNow, 1);
-				btnJumpTwo.setClickable(true);
-				btnJumpOne.setClickable(true);
-			}
-		});
-
-		linearLayout.startAnimation(as);
-		Log("Jump One");
+		jumpStep = count;
+		jumpThread = new Thread(new JumpThread());
+		jumpThread.start();
+		setBtn(false);
 	}
 
-	protected void jumpTwo() {
-		animDrawable.stop();
-		animDrawable.start();
-		mapNow += 2;
-		AnimationSet as = new AnimationSet(true);
-		TranslateAnimation al = new TranslateAnimation(0, -(mapNow * 132), 0,
-				-(mapNow * 132) - 2 * 132, 0, 0, 0, 0);
-		al.setDuration(280);
-		TranslateAnimation al2 = new TranslateAnimation(0, -132 * 2, 0, 0, 0,
-				0, 0, 0);
-		al2.setDuration(0);
-		al2.setStartOffset(280);
-		as.addAnimation(al);
-		// as.addAnimation(al2);
-		as.setFillAfter(true);
-		al.setAnimationListener(new AnimationListener() {
-			@Override
-			public void onAnimationStart(Animation animation) {
-				btnJumpOne.setClickable(false);
-				btnJumpTwo.setClickable(false);
-			}
+	private void setBtn(boolean canuse) {
+		btnJumpOne.setClickable(canuse);
+		btnJumpTwo.setClickable(canuse);
+	}
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
 
-			@Override
-			public void onAnimationRepeat(Animation animation) {
-			}
 
-			@Override
-			public void onAnimationEnd(Animation animation) {
-				// initMap(mapNow, 2);
-				btnJumpTwo.setClickable(true);
-				btnJumpOne.setClickable(true);
+	class BackgroudThread implements Runnable {
+		@Override
+		public void run() {
+			try {
+				// TODO Auto-generated method stub
+				while (running && a < 1000) {
+					Canvas canvas = holder.lockCanvas();
+					canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+					canvas.drawBitmap(zhaoze01, a, 0, null);
+					holder.unlockCanvasAndPost(canvas);
+					holder.lockCanvas(new Rect(0, 0, 0, 0));
+					holder.unlockCanvasAndPost(canvas);
+					a++;
+					Thread.sleep(100);
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			} finally {
+				// holder.unlockCanvasAndPost(canvas);
 			}
-		});
-		linearLayout.startAnimation(as);
-		Log("Jump Two");
+		}
+	}
+
+
+	class JumpThread implements Runnable {
+		@Override
+		public void run() {
+			int move = 0;
+			int[] map = initObject(mapNow);
+			boolean init = mapNow == 0 ? true : false;
+			try {
+				// TODO Auto-generated method stub
+				while (move > -132 * jumpStep || init) {
+					Canvas canvas = holder.lockCanvas();
+					canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+					Bitmap temmap = zhaoze01;
+					for (int tem = 0; tem < map.length; tem++) {
+						boolean skip = false;
+						switch (map[tem]) {
+						case 0:
+							skip=true;
+							break;
+						case 1:
+							temmap = zhaoze01;
+							break;
+						case 2:
+							temmap = zhaoze02;
+							break;
+						case 3:
+							temmap = zhaoze03;
+							break;
+						case 4:
+							temmap = zhaoze04;
+							break;
+						}
+						if (!skip) {
+							canvas.drawBitmap(temmap, tem * 132 + move, 0, null);
+						}
+
+					}
+					holder.unlockCanvasAndPost(canvas);
+					holder.lockCanvas(new Rect(0, 0, 0, 0));
+					holder.unlockCanvasAndPost(canvas);
+					move = move - 16 * jumpStep;
+					init = false;
+					Thread.sleep(5);
+				}
+				mapNow += jumpStep;
+				setBtn(true);
+			} catch (Exception e) {
+				// TODO: handle exception
+			} finally {
+				// holder.unlockCanvasAndPost(canvas);
+			}
+		}
 	}
 }
